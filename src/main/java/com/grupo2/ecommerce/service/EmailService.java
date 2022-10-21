4c648c4f9fdd808a483b56cb1e13c69a8a326d70
jonnyband <jmsupernet1@gmail.com>
2022-10-21 15:00:50 -0300

    Correção do EmailConfig

--- src/main/java/com/grupo2/ecommerce/service/EmailService.java
+++ src/main/java/com/grupo2/ecommerce/service/EmailService.java
@@ -4,6 +4,8 @@ import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
